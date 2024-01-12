a6290db2e63141898811411b694ae4aa5b9c9771
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-01-12 14:49:21 +0300

    commited

--- cafe/src/app/app.routes.ts
+++ cafe/src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+path: '',
+redirectTo: 'home',
+pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'search',
+        component: SearchComponent
+    } ,
+      {
+        path: 'bookings',
+        component: BookingsComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    }
+];
+
+
+
+
